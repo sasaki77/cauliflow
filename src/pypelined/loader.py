b16a27a93f922f08d2a07bcddc1c60f2b4398a1d
Shinya Sasaki <shinya.sasaki@kek.jp>
2025-05-02 14:20:13 +0900

    ENH: add yaml config loader

---
+++ src/pypelined/loader.py
@@ -0,0 +1,98 @@
+from enum import StrEnum
+from logging import getLogger
+
+import yaml
+
+from pypelined.flow import ConcurrentFlows, Flow, Flows, SequentialFlows
+from pypelined.macros import Macros, macros
+
+_logger = getLogger(__name__)
+
+
+class FlowsType(StrEnum):
+    SEQUENCTIAL = "sequential"
+    CONCURRENT = "concurrent"
+
+
+def flow_from_yaml(file_path) -> Flows:
+    yaml_dict = _load_yaml(file_path)
+
+    is_seq = FlowsType.SEQUENCTIAL in yaml_dict
+    is_con = FlowsType.CONCURRENT in yaml_dict
+    if is_seq and is_con:
+        _logger.error("multiple flows are detected")
+
+    flows = None
+    if is_seq:
+        flows = _make_seq(yaml_dict[FlowsType.SEQUENCTIAL])
+    elif is_con:
+        flows = _make_con(yaml_dict[FlowsType.CONCURRENT])
+    else:
+        _logger.error("flows not defined")
+
+    if "macros" in yaml_dict:
+        mcr = Macros()
+        mcr.update(yaml_dict["macros"])
+        macros.set(mcr)
+
+    return flows
+
+
+def _make_flows(flows: dict) -> Flows:
+    flow_list = []
+    for flow in flows:
+        new_flow = None
+        if "flow" in flow:
+            new_flow = _make_flow(flow)
+        elif FlowsType.SEQUENCTIAL in flow:
+            new_flow = _make_seq(flow)
+        elif FlowsType.CONCURRENT in flow:
+            new_flow = _make_con(flow)
+        else:
+            continue
+        flow_list.append(new_flow)
+    return flow_list
+
+
+def _make_seq(config: dict):
+    if "flows" not in config:
+        _logger.error("no flows in sequential flow")
+    flows = SequentialFlows()
+    flow_list = _make_flows(config["flows"])
+    flows.extend(flow_list)
+    return flows
+
+
+def _make_con(config: dict):
+    if "flows" not in config:
+        _logger.error("no flows in concurrent flow")
+    flows = ConcurrentFlows()
+    flow_list = _make_flows(config["flows"])
+    flows.extend(flow_list)
+    return flows
+
+
+def _make_flow(config: dict):
+    if "flow" not in config:
+        _logger.error("no flow in flow")
+    flow = Flow()
+    prev_node = "root"
+    for node in config["flow"]:
+        if len(node) != 1:
+            _logger.error("node read error")
+        for node_type, params in node.items():
+            parent = prev_node
+            if "name" not in params:
+                params["name"] = node_type
+            if "parent" in params:
+                parent = params["parent"]
+                del params["parent"]
+            flow.create_node(node_type, _parent=parent, **params)
+            prev_node = params["name"]
+    return flow
+
+
+def _load_yaml(file_path):
+    with open(file_path, "r") as file:
+        data = yaml.safe_load(file)
+    return data
