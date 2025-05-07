3425c9b6852fa327856236fac092df4def65a61c
Shinya Sasaki <shinya.sasaki@kek.jp>
2025-05-07 16:51:07 +0900

    ENH: use context vars on logging

--- src/pypelined/logging.py
+++ src/pypelined/logging.py
@@ -1,9 +1,25 @@
 import logging
 
+from pypelined.context import ctx_flow, ctx_node
+
+
+class InjectingFilter(logging.Filter):
+    def filter(self, record):
+        flow = ctx_flow.get()
+        node = ctx_node.get()
+        record.flow_name = flow.name
+        record.node_name = node.name
+        return True
+
+
 default_handler = logging.StreamHandler()
 default_handler.setFormatter(
-    logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(flow_name)s - %(node_name)s %(message)s"
+    )
 )
+
+default_handler.addFilter(InjectingFilter())
 
 
 def get_logger(name: str) -> logging.Logger:
