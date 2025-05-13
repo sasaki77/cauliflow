31d0534dadc628544c6af561d84253eb4017df76
Shinya Sasaki <shinya.sasaki@kek.jp>
2025-05-13 15:31:29 +0900

    TST: add tests for itemloop node

---
+++ src/pypelined/tests/plugins/test_itemloop.py
@@ -0,0 +1,35 @@
+import pytest
+
+from pypelined.context import ctx_flowdata
+from pypelined.plugins.itemloop import ForDict, ForList
+
+
+@pytest.mark.asyncio
+async def test_for_list():
+    params = {
+        "lists": [[1, 2], [3, 4]],
+        "expression": "item0*item1",
+        "filter": "item0*item1>7",
+    }
+    node = ForList(name="forlist", param_dict=params)
+    await node.run()
+    flowdata = ctx_flowdata.get()
+    assert flowdata["forlist"] == [3, 4, 6]
+
+
+@pytest.mark.asyncio
+async def test_for_dict():
+    params = {
+        "lists": [["head1", "head2"], {"name1": "val1", "name2": "val2"}],
+        "key": "item0 + ':' + item1_key",
+        "val": "item1_val",
+    }
+    node = ForDict(name="fordict", param_dict=params)
+    await node.run()
+    flowdata = ctx_flowdata.get()
+    assert flowdata["fordict"] == {
+        "head1:name1": "val1",
+        "head1:name2": "val2",
+        "head2:name1": "val1",
+        "head2:name2": "val2",
+    }
