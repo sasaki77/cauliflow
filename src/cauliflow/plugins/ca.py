01646771c32e59bc130e1dc4dae9a478444ee6f8
Shinya Sasaki <shinya.sasaki@kek.jp>
2025-06-09 10:46:06 +0900

    ENH: fix ca plugin with aioca

--- src/cauliflow/plugins/ca.py
+++ src/cauliflow/plugins/ca.py
@@ -1,7 +1,7 @@
 from functools import singledispatch
 
-import epics
 import janus
+from aioca import caget, camonitor
 
 from cauliflow.context import ctx_flowdata, init_flowdata
 from cauliflow.node import ArgSpec, ProcessNode, TriggerNode, node
@@ -9,6 +9,27 @@ from cauliflow.node import ArgSpec, ProcessNode, TriggerNode, node
 
 @node.register("camonitor")
 class CamonitorNode(TriggerNode):
+    """
+    DOCUMENTATION:
+      short_description: Monitor EPICS PV(s) and pass it flowdata.
+      description:
+        - Monitor EPICS PV(s) and pass it flowdata.
+      parameters:
+        pvname:
+          description:
+            - A pvname or a list of pvname.
+    EXAMPLE: |-
+      # Get single pv data.
+      # Output: {'name': 'TEST:PV1', 'value': 7.0, 'timestamp': 1749196716.822903, 'status': 0, 'severity': 0, 'ok': True}
+      - camonitor:
+          pvname: "TEST:PV1"
+
+      # Get list of pv data.
+      # Output: {'name': 'TEST:PV1', 'value': 7.0, 'timestamp': 1749196716.822903, 'status': 0, 'severity': 0, 'ok': True}
+      - camonitor:
+          pvname: ["TEST:PV1", "TEST:PV2"]
+    """
+
     def __init__(self, name: str, param_dict: dict | None = None):
         super().__init__(name, param_dict)
         self.q = janus.Queue()
@@ -19,24 +40,28 @@ class CamonitorNode(TriggerNode):
             "pvname": ArgSpec(type="any", required=True),
         }
 
-    def onChanges(
-        self, pvname=None, value=None, timestamp=None, status=None, severity=None, **kw
-    ):
-        data = {
-            "name": pvname,
-            "value": value,
-            "timestamp": timestamp,
-            "status": status,
-            "severity": severity,
-        }
+    def callback(self, val, _):
+        if val.ok:
+            data = {
+                "name": val.name,
+                "value": val,
+                "timestamp": val.timestamp,
+                "status": val.status,
+                "severity": val.severity,
+                "ok": True,
+            }
+        else:
+            data = {"name": val.name, "ok": False}
+
         self.q.sync_q.put(data)
 
     async def process(self):
         pvnames = self.params["pvname"]
+        pvnames = _get_pvnames(pvnames)
 
-        for pvname in pvnames:
-            pv = epics.get_pv(pvname, callback=self.onChanges)
-            self.pvs.append(pv)
+        camonitor(
+            pvnames, self.callback, format=1, all_updates=True, notify_disconnect=True
+        )
 
         while True:
             pvdata = await self.q.async_q.get()
@@ -50,33 +75,74 @@ class CamonitorNode(TriggerNode):
 
 @node.register("caget")
 class CagetNode(ProcessNode):
-    def __init__(self, name: str, param_dict: dict | None = None):
-        super().__init__(name, param_dict)
-        self.q = janus.Queue()
-        self.pvs: list[epics.PV] | None = None
+    """
+    DOCUMENTATION:
+      short_description: Get EPICS PV(s) and pass it flowdata.
+      description:
+        - Get EPICS PV(s) and pass it flowdata.
+      parameters:
+        pvname:
+          description:
+            - A pvname or a list of pvname.
+        timeout:
+          description:
+            - Wait time.
+    EXAMPLE: |-
+      # Get single pv data.
+      # Output: {'name': 'TEST:PV1', 'value': 7.0, 'timestamp': 1749196716.822903, 'status': 0, 'severity': 0, 'ok': True}
+      - caget:
+          pvname: "TEST:PV1"
+
+      # Get list of pv data.
+      # Output: [
+      #  {'name': 'TEST:PV1', 'value': 7.0, 'timestamp': 1749196716.822903, 'status': 0, 'severity': 0, 'ok': True},
+      #  {'name': 'TEST:PV2', 'value': 8.0, 'timestamp': 1749196717.822903, 'status': 0, 'severity': 0, 'ok': True},
+      # ]
+      - caget:
+          pvname: ["TEST:PV1", "TEST:PV2"]
+
+      # Get single pv data with timeout.
+      # Timeout Output: {'name': 'TEST:PV1', 'ok': False}
+      - caget:
+          pvname: "TEST:PV1"
+          timeout: 1.0
+    """
 
     def set_argument_spec(self) -> dict[str, ArgSpec]:
         return {
-            "pvname": ArgSpec(type="any", required=True),
+            "pvname": ArgSpec(type="str|list[str]", required=True),
+            "timeout": ArgSpec(type="float", required=False, default=5.0),
         }
 
     async def process(self):
-        if self.pvs is None:
-            pvnames = self.params["pvname"]
-            pvnames = _get_pvnames(pvnames)
+        pvnames = self.params["pvname"]
+        is_single = isinstance(pvnames, str)
+        pvnames = _get_pvnames(pvnames)
 
-            self.pvs = []
-            for pvname in pvnames:
-                pv = epics.get_pv(pvname)
-                self.pvs.append(pv)
+        vals = await caget(
+            pvnames, format=1, timeout=self.params["timeout"], throw=False
+        )
 
         out = []
-        for pv in self.pvs:
-            val = pv.get()
-            out.append({"pvname": pv.pvname, "val": val})
+        for val in vals:
+            if val.ok:
+                data = {
+                    "name": val.name,
+                    "value": val,
+                    "timestamp": val.timestamp,
+                    "status": val.status,
+                    "severity": val.severity,
+                    "ok": True,
+                }
+            else:
+                data = {"name": val.name, "ok": False}
+            out.append(data)
+
+        if is_single:
+            out = out[0]
+
         fd = ctx_flowdata.get()
         fd[self.name] = out
-        return
 
 
 @singledispatch
